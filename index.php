d59dfe92b94ae8d97f2c851e078e9f9f09523c7e
priscila <pr.floriano88@gmail.com>
2020-07-12 03:21:50 -0300

    alterações no layout

---
+++ index.php
@@ -0,0 +1,29 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+    <link rel="stylesheet" href="./css/login.css">
+    <title>Login</title>
+</head>
+<body class="bg-login">
+    <section class="login">
+        <div class="image-login">
+            <img src="./image/logo.png" alt="">
+        </div>
+
+        <form action="" class="form-login">
+            <input type="text" name="email" placeholder="Email" required>
+            <input type="password" name="senha" placeholder="Senha" required>
+
+            <button class="btn-login">Acessar</button>
+            <a href="" class="rec-pass">Esqueci a senha</a>
+
+        </form>
+    </section>
+
+</body>
+</html>
